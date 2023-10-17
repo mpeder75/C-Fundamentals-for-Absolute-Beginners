7eb484f9c2c47acd7792938360399ca4731bec7e
Mpeder <mpeder75@gmail.com>
2023-10-17 17:11:29 +0200

    Section 1-7

--- 6. Variables/Variables.cs
+++ 6. Variables/Variables.cs
@@ -1,15 +1,53 @@
 ﻿
-// Variabel er et område i memory der reserveres
+// Variabel oprettes og reservere et område i memory
 // Variabler skal ses som en container for noget data
 
-// Man kan opbevare:
-//  • int  - tal
-//  • string  - tekst
-//  • DateTime - date objekter 
-
+/* 
+    Man kan opbevare:
+    • int  - tal
+    • string  - tekst
+    • DateTime - date objekter 
+*/
 
 // Man declare datatype på variabel også navn på variabel 
 int x = 7;
 int y = x + 3;
 
 Console.WriteLine(y);
+
+
+
+/* ----------- Strings ----------- */
+
+string myFirtstName = "Michael";
+Console.WriteLine(myFirtstName);
+
+
+/* var datatype:
+   compiler finder selv ud af hvilken datatype 'myFirstName' er
+   var kræver at når der declares skal der assignes en værdi */
+var myFirstName = "Michael";
+
+
+
+/* ----------- Konvertere en datatype til en anden ----------- */
+
+
+// konverter en int til en string med ToString()
+int a = 7;
+string b = "bob";
+
+string myFirstTry = a.ToString() + b;
+
+Console.WriteLine(myFirstTry);
+
+
+
+// konverter strig til int med int.Parse
+
+int d = 7;
+string e = "5";
+
+int mySecondTry = d + int.Parse(e);
+
+Console.WriteLine(mySecondTry);
---
+++ 7. If statements/Program.cs
@@ -0,0 +1,57 @@
+﻿
+/*
+// Input fra brugerr
+Console.WriteLine("Please type something");
+string userInput = Console.ReadLine();
+
+Console.WriteLine($"You entered: {userInput}");*/
+
+
+/*
+Console.WriteLine("Would you prefer door number: 1, 2, or 3? ");
+string userValue = Console.ReadLine();
+
+if (userValue == "1")
+{
+    Console.WriteLine($"You won a new car ");
+} else if (userValue == "2")
+{
+    Console.WriteLine($"There is nothing behind the door");
+} else if (userValue == "3")
+{
+    Console.WriteLine($"You won a free meal");
+} else
+{
+    Console.WriteLine("Invalid input");
+}
+*/
+
+
+Console.WriteLine("Choose door: 1, 2 or 3!");
+string userValue = Console.ReadLine();
+/*
+string msg = "";
+
+if(userValue == "1")
+{
+    msg = "You won a boat!";
+} else if (userValue == "2")
+{
+    msg = "The door is empty";
+} else if (userValue == "3")
+{
+    msg = "You won a cat";
+} else
+{
+    msg = "Invalid input";
+}
+
+Console.WriteLine(msg);
+*/
+
+// ------------ Conditional operator ---------- //
+
+// Hvis userValue er 1 sættes message til 'boat'
+// ellers sættes message til 'Halloo'
+string message = (userValue == "1") ? "boat" : "Halloo";
+Console.WriteLine("You won a {0}", message);
